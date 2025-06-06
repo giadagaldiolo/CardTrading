9496144393a0a247394b403441b4f8654e84a095
giadagaldiolo <giada.galdiolo@student.supsi.ch>
2025-06-06 09:56:09 +0200

    try to fix Failed to load initial card images

--- src/main/java/ch/supsi/web/cardgames/service/DbInitializerService.java
+++ src/main/java/ch/supsi/web/cardgames/service/DbInitializerService.java
@@ -62,10 +62,10 @@ public class DbInitializerService {
         if(count == 0) {
             try {
                 cardService.saveCard(new Card("Ash", "Bulbasaur", "Bulbasaur description", new Date(), Files.readAllBytes(Paths.get(uploadPath, "Bulbasaur.png")), CardCondition.MINT, CardType.POKEMON, users.get(1)));
-                cardService.saveCard(new Card("Ash", "Pikachu", "Pikachu description", new Date(), Files.readAllBytes(Paths.get(uploadPath, "pikachu.jpg")), CardCondition.MINT, CardType.POKEMON, users.get(1)));
                 cardService.saveCard(new Card("Ash","Charmander", "Charmander description", new Date(), Files.readAllBytes(Paths.get(uploadPath, "Charmander.jpg")), CardCondition.NEAR_MINT, CardType.POKEMON, users.get(1)));
                 cardService.saveCard(new Card("Kaiba", "Blue-Eyes White Dragon", "Blue-Eyes White Dragon descriptiobn", new Date(), Files.readAllBytes(Paths.get(uploadPath, "Blue-Eyes-Jet-Dragon.jpg")), CardCondition.MINT, CardType.YU_GI_OH, users.get(2)));
                 cardService.saveCard(new Card("MtG Player", "Swords to Plowshares", "Swords to Plowshares description", new Date(), Files.readAllBytes(Paths.get(uploadPath, "swords-to-plowshares.jpg")), CardCondition.NEAR_MINT, CardType.MAGIC, users.get(2)));
+                cardService.saveCard(new Card("Ash", "Pikachu", "Pikachu description", new Date(), Files.readAllBytes(Paths.get(uploadPath, "pikachu.jpg")), CardCondition.MINT, CardType.POKEMON, users.get(1)));
             } catch (IOException e) {
                 throw new RuntimeException("Failed to load initial card images", e);
             }
